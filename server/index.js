0e355f0b2642fd0f57c6da66253e2bfdac2e2d4e
mcarson01 <matthcarson01@gmail.com>
2017-10-18 17:38:24 -0500

    Afternoon Project

---
+++ server/index.js
@@ -0,0 +1,19 @@
+//Require express and body- parser
+const express = require('express');
+const bodyParser = require('body-parser');
+const mc = require(__dirname + '/controllers/messages_controller');
+
+app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+});
